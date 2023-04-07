4cbd991b4166216a7c95caf86d338f372230dd4c
Mark Bernardo <>
2023-04-07 11:47:07 -0700

    Fix playerChoice buttons and add audio

--- app.js
+++ app.js
@@ -16,11 +16,16 @@ const playerPrompt = document.querySelector(".prompt");
 //Default States
 let power = 0;
 let playerSelection;
+let computerChoice;
+
+
+let powerOnSound = new Audio("audio/power-on-sound.mp3");
+let powerOffSound = new Audio("audio/power-off-sound.mp3");
 
 //Testers
 
 
-powerOnOff();
+powerSwitch();
 
 //Text Flash Script
 /* let screenTextFlash =  setInterval(() => {
@@ -31,56 +36,123 @@ powerOnOff();
 clearInterval(screenTextFlash); */
 
 
-function powerOnOff () {
+function powerSwitch () {
     screenText.textContent = "< Turn on Console to Play >";
 
     powerButton.addEventListener("click", () => {
         if (power == 0) {
-            screenText.classList.add("invisible");
-            screenText.textContent = "Welcome to Rock Paper Scissors!";
-            setTimeout(() => {
-                screenText.classList.remove("invisible");
-                for (var i = 0; i < playerCard.length; i++) {
-                    playerCard[i].classList.remove("invisible");
-                    };
-            }, 3000);
-            power++;
+            powerOnSequence();
             console.log(power);
+            powerOnSound.play();
         }   else {
-            screenText.textContent = "Powering off..";
-            screenText.classList.remove("invisible");
-            setTimeout(() => {
-                screenText.textContent = "< Turn on Console to Play >";
-                for (var i = 0; i < playerCard.length; i++) {
-                    playerCard[i].classList.add("invisible");
-                    };
-            }, 2000);
-            power--;
+            powerOffSequence();
             console.log(power);
+            powerOffSound.play();
         }
     });
 };
 
+
+function powerOnSequence() {
+    screenText.classList.add("invisible");
+    screenText.textContent = "Welcome to Rock Paper Scissors!";
+    setTimeout(() => {
+        screenText.classList.remove("invisible");
+        for (var i = 0; i < playerCard.length; i++) {
+            playerCard[i].classList.remove("invisible");
+            };
+    }, 1500);
+    power++;
+    roundCounter();
+}
+
+function powerOffSequence() {
+    screenText.textContent = "Powering off..";
+    screenText.classList.remove("invisible");
+    setTimeout(() => {
+        screenText.textContent = "< Turn on Console to Play >";
+        for (var i = 0; i < playerCard.length; i++) {
+            playerCard[i].classList.add("invisible");
+            };
+    }, 1500);
+    power--;
+}
 
 
 
 
 /* Script for RPS */
 //Create an array to store RPS
-const object = ["rock", "paper", "scissors"];
+const objectChoices = ["rock", "paper", "scissors"];
 
 //Make function for getComputerChoice() and use the array
 function getComputerChoice() {
-    return object[~~Math.random() * object.length];
+    return objectChoices[~~Math.random() * objectChoices.length];
 }
 
 //Make function for getPlayerChoice() utilizing id
-playerCard.forEach((button) => {
-    button.addEventListener("click", () => {
-        const emoji = button.querySelector(".player-card");
-        playerChoice = emoji.alt;
+playerCard.forEach((card) => {
+    card.addEventListener("click", () => {
+        const objectChoice = card.getAttribute("id");
+        playerChoice = objectChoice;
+        console.log(playerChoice);
+
+        gamePlay(playerChoice, computerChoice);
     })
 })
 
 //Round function for gameplay
+function gamePlay (playerChoice, computerChoice) {
+    computerChoice = getComputerChoice().toLowerCase();
+    playerChoice = playerChoice.toLowerCase();
+
+    if (playerChoice == "rock" && computerChoice == "scissors") {
+        screenText.textContent = ("You Win! Rock beats Scissors!");
+        screenText.classList.remove("invisible");
+    } else if (playerChoice == "rock" && computerChoice == "paper") {
+        screenText.textContent = ("You Lose! Paper beats Rock!");
+        screenText.classList.remove("invisible");
+    } else if (playerChoice == "paper" && computerChoice == "rock") {
+        screenText.textContent = ("You Win! Paper beats Rock!");
+        screenText.classList.remove("invisible");
+    } else if (playerChoice == "paper" && computerChoice == "scissors") {
+        screenText.textContent = ("You Lose! Scissors beats Paper!");
+        screenText.classList.remove("invisible");
+    } else if (playerChoice == "scissors" && computerChoice == "paper") {
+        screenText.textContent = ("You Win! Scissors beats Paper!");
+        screenText.classList.remove("invisible");
+    } else if (playerChoice == "scissors" && computerChoice == "rock") {
+        screenText.textContent = ("You Lose! Rock beats Scissors!");
+        screenText.classList.remove("invisible");
+    } else {
+        screenText.textContent = ("Draw.");
+        screenText.classList.remove("invisible");
+    }
+}
+
+//Round-counter
+
+let round = 0;
+
+function roundCounter() {
+    screenText.classList.add("invisible");
+    let roundNumber;
+
+    if (round = 0) {
+        roundNumber = 1;
+    } else if (round = 1) {
+        roundNumber = 2;
+    } else if (round = 2) {
+        roundNumber = 3;
+    } else if (round = 3) {
+        roundNumber = 4;
+    } else {
+        roundNumber = 5;
+    }
+
+    console.log(round);
+    screenText.textContent = (`Round ${roundNumber}`);
+    screenText.classList.remove("invisible");
+}
+
 //Results
